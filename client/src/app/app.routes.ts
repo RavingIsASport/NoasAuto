59b33c2d741ac4671f0abd5ac34e6d394e08ee05
Gustavo Garcia <gustavogarcia42@yahoo.com>
2025-02-28 18:26:18 -0600

    fixed script

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -15,14 +15,13 @@ export const routes: Routes = [
         path: 'home',
         component: HomeComponent,
       },
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-      },
     ],
   },
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   {
     path: 'admin',
     component: AdminLayoutComponent,
--- client/src/app/layouts/admin-layout/admin-layout.component.ts
+++ client/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [LoginComponent, AdminComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
