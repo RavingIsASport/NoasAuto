82ed3f70b9428b499498fbae05772f2fae8b82e0
Gustavo Garcia <gustavogarcia42@yahoo.com>
2025-03-10 19:38:19 -0500

    styled the navbar for mobile and desktop view. As well as added a hero section with call to action buttons.

--- client/src/app/layouts/main-layout/navbar/navbar.component.ts
+++ client/src/app/layouts/main-layout/navbar/navbar.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  // nav is used to toggle the mobile nav
+  @ViewChild('navLinks') nav!: ElementRef;
+
+  // renderer is used to add and remove classes
+  constructor(private renderer: Renderer2) {}
+
+  // showIcon is used to toggle the mobile nav icon
+  showIcon = true;
+
+  // navToggle is used to toggle the
+  // mobile nav when the icon is clicked
+  navToggle() {
+    if (this.nav.nativeElement.classList.contains('hidden')) {
+      this.renderer.removeClass(this.nav.nativeElement, 'hidden');
+      this.showIcon = false;
+    } else {
+      this.renderer.addClass(this.nav.nativeElement, 'hidden');
+      this.showIcon = true;
+    }
+  }
+}
