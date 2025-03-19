e84210f4c8a7a0f7aede510e4e811cb5df69534b
Gustavo Garcia <gustavogarcia42@yahoo.com>
2025-03-18 19:28:03 -0500

    styled finance page for mobile view

--- client/src/app/layouts/main-layout/finance/finance.component.ts
+++ client/src/app/layouts/main-layout/finance/finance.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-finance',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './finance.component.html',
-  styleUrl: './finance.component.css'
+  styleUrl: './finance.component.css',
 })
-export class FinanceComponent {
-
-}
+export class FinanceComponent {}
